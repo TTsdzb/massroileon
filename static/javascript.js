efd0f8cdac17e76adfdbd9b91ee1efcd09bf7ccd
MassroiLeon <149990805+MassroiLeon@users.noreply.github.com>
2024-02-26 22:37:26 +0700

    Update javascript.js

--- static/javascript.js
+++ static/javascript.js
@@ -160,6 +160,12 @@ const data = [
     displayName: "ok boomber",
     donated: "Full",
   },
+  {
+    id: "0025",
+    displayName: "Duy Nguyen Minh Dang",
+    donated: "70K/512K",
+    dueDate: "Mar 28, 2024 00:00:00",
+  },
 ];
 
 document.addEventListener("DOMContentLoaded", function () {

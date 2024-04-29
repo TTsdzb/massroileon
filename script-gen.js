571ff3b00fede8eab6f252802bffe67d6520a334
MassroiLeon <149990805+MassroiLeon@users.noreply.github.com>
2024-04-29 09:20:16 +0700

    Update script-gen.js

--- script-gen.js
+++ script-gen.js
@@ -114,7 +114,7 @@ function addSong() {
     if (audioOverride) newSong.difficulties[3].audioOverride = true;
 
     // Kiểm tra và loại bỏ thông tin về các khó khăn nếu không được nhập vào
-    if (ratingClass3.trim() === '') {
+    if (rating3.trim() === null) {
         newSong.difficulties.pop(); // Loại bỏ phần tử cuối cùng khỏi mảng difficulties
     }
 
